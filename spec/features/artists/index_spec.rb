1afb1a9882a3ca7eec6f1936a0ab81a85e608a30
Isabelle Villasenor <villasenori@outlook.com>
2021-01-01 08:30:41 -0700

    Add Artist Index Feature Spec For Linking to Creating A New Artist

--- spec/features/artists/index_spec.rb
+++ spec/features/artists/index_spec.rb
@@ -4,23 +4,22 @@ RSpec.describe 'As A Visitor' do
   before :each do
     @sc = Artist.create!(name: "Scott Colley")
     @ys = Artist.create!(name: "Yasuaki Shimizu")
+    visit artists_path
   end
-  describe 'When I Visit The Artist Index' do
-    it 'show all artists and has the title Isabelles Artists' do
-      visit "/artists"
 
+  describe 'When I Visit The Artist Index' do
+
+    it 'show all artists and has the title Isabelles Artists' do
       expect(page).to have_content("Isabelles Artists")
       expect(page).to have_content("#{@sc.name}")
       expect(page).to have_content("#{@ys.name}")
     end
 
     it 'has link to create a new artist' do
-      visit "/artists"
-
       expect(page).to have_link("Create New Artist")
       click_link
 
-      expect(current_path).to eq ("/artists/new")
+      expect(current_path).to eq (new_artist_path)
     end
   end
 end
