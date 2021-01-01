6f436fff76b5a422efd73fed078f8615700b17bb
Isabelle Villasenor <villasenori@outlook.com>
2021-01-01 09:53:51 -0700

    Add Edit Artist Index Feature Spec

--- spec/features/artists/index_spec.rb
+++ spec/features/artists/index_spec.rb
@@ -1,42 +1,53 @@
 require 'rails_helper'
 
-describe 'As A Visitor' do
-  before :each do
+describe 'Artist Index' do
+  it 'should show all artists and has the title Isabelles Artists' do
+    @sc = Artist.create!(name: 'Scott Colley')
+    @ys = Artist.create!(name: 'Yasuaki Shimizu')
+
     visit artists_path
+
+    expect(page).to have_content('Isabelles Artists')
+    expect(page).to have_content("#{@sc.name}")
+    expect(page).to have_content("#{@ys.name}")
   end
-  
-  describe 'When I Visit The Artist Index' do
-    
-    it 'show all artists and has the title Isabelles Artists' do
-      @sc = Artist.create!(name: 'Scott Colley')
-      @ys = Artist.create!(name: 'Yasuaki Shimizu')
 
-      expect(page).to have_content('Isabelles Artists')
-      expect(page).to have_content("#{@sc.name}")
-      expect(page).to have_content("#{@ys.name}")
-    end
+  it 'should have link to create a new artist' do
+    visit artists_path
 
-    it 'has link to create a new artist' do
-      expect(page).to have_link('Create New Artist')
-      click_link 'Create New Artist'
+    expect(page).to have_link('Create New Artist')
+    click_link 'Create New Artist'
 
-      expect(current_path).to eq (new_artist_path)
-      fill_in :name, with: 'Nobuo Uematsu'
-      click_button 'Create Artist'
+    expect(current_path).to eq (new_artist_path)
+    fill_in :name, with: 'Nobuo Uematsu'
+    click_button 'Create Artist'
 
-      expect(current_path).to eq (artists_path)
-      expect(page).to have_content('Nobuo Uematsu')
-    end
+    expect(current_path).to eq (artists_path)
+    expect(page).to have_content('Nobuo Uematsu')
+  end
 
-    it 'has a link to destroy an artist' do
-      @ys = Artist.create!(name: 'Yasuaki Shimizu')
+  it 'should have a link to destroy an artist' do
+    @ys = Artist.create!(name: 'Yasuaki Shimizu')
 
-      expect(page).to have_content("#{@ys.name}")
-      expect(page).to have_button('Delete')
-      click_button 'Delete'
+    visit artists_path
 
-      expect(current_path).to eq(artists_path)
-      expect(page).to_not have_content("#{@ys.name}")
-    end
+    expect(page).to have_content("#{@ys.name}")
+    expect(page).to have_button('Delete')
+    click_button 'Delete'
+
+    expect(current_path).to eq(artists_path)
+    expect(page).to_not have_content("#{@ys.name}")
+  end
+
+  it 'should have a link to edit an artist' do
+    @ys = Artist.create!(name: 'Yasuaki Shimizu')
+
+    visit artists_path
+
+    expect(page).to have_content("#{@ys.name}")
+    expect(page).to have_button('Edit')
+    click_button 'Edit'
+
+    expect(current_path).to eq(edit_artist_path(@ys))
   end
 end
