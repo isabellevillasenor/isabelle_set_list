0ab80b500f7f7249b9f89deeb9f9e4d37e79309f
Isabelle Villasenor <villasenori@outlook.com>
2021-01-01 08:25:56 -0700

    Add Validations to Artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,7 @@
 class Artist < ApplicationRecord
   has_many :songs
+  validates_presence_of :name
+  validates_uniqueness_of :name, with: :message
 
   def average_song_length
     songs.average(:length)
