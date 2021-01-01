4792a6501b5710e6006c3618183e1b320a6f33b0
Isabelle Villasenor <villasenori@outlook.com>
2021-01-01 09:56:24 -0700

    Add Edit, Update and find_artist Before Action to Controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,11 @@
 class ArtistsController < ApplicationController
+  before_action :find_artist, only: [:show, :destroy, :edit, :update]
+
   def index
     @artists = Artist.all
   end
   
   def show
-    @artist = Artist.find(params[:id])
   end
 
   def new
@@ -16,11 +17,24 @@ class ArtistsController < ApplicationController
   end
 
   def destroy
-    Artist.destroy(params[:id])
+    @artist.destroy 
+    redirect_to artists_path
+  end
+
+  def edit
+  end
+
+  def update
+    @artist.update(artist_params)
+    flash.notice = "#{@artist.name} Updated!"
     redirect_to artists_path
   end
 
   private
+  def find_artist
+    @artist = Artist.find(params[:id])
+  end
+
   def artist_params
     params.permit(:name)
   end
