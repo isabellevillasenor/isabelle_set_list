ff1b6b98a941e9619d05606895216c82ee00b1ad
Isabelle Villasenor <villasenori@outlook.com>
2021-01-01 08:53:35 -0700

    Refactor Syntax in New Artist Spec

--- spec/features/artists/new_spec.rb
+++ spec/features/artists/new_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe 'As A Visitor' do
   describe 'When I Visit The New Artist Form' do
     it 'should be able to create a new artist by filling in the name and clicking link' do
-      visit "/artists/new"
+      visit new_artist_path
 
-      expect(page).to have_button("Create Artist")
+      expect(page).to have_button('Create Artist')
 
       fill_in 'Name', with: 'Hosono'
       click_button 
 
-      expect(current_path).to eq("/artists")
+      expect(current_path).to eq(artists_path)
       expect(page).to have_content('Hosono')
     end
   end
